aefc50ad7020e3607ae7936ed82a2ee9b09d78d3
sarahp <me@sarahp.studio>
2021-01-25 19:00:31 -0500

    Setting up firebase config settings.

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "spstudio-a2f09.firebaseapp.com",
+  databaseURL: "https://spstudio-a2f09.firebaseio.com",
+  projectId: "spstudio-a2f09",
+  storageBucket: "spstudio-a2f09.appspot.com",
+  messagingSenderId: "1054898651414",
+  appId: "1:1054898651414:web:09b02d4caedd6c74f4d037"
+};
