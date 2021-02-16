953d1caa9fbd40e651ed4f8e5d7a693c0a3e82e1
sarahp <me@sarahp.studio>
2021-02-16 12:50:29 -0500

    Cleaning up resume text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,17 +25,19 @@ const IndexPage = () => {
                 </div>
               <div className="header-inner">
                   <div className="social-links">
-                    <Link href="https://www.linkedin.com/in/sarahprasuhn/" target="_blank" rel="noopener noreferrer"><img className="social-img linked-in align-bottom" alt="A Linkedin logo in blue and gray." src={linkedin} /></Link>
+                    <a href="https://www.linkedin.com/in/sarahprasuhn/" target="_blank" rel="noopener noreferrer"><img className="social-img linked-in align-bottom" alt="A Linkedin logo in blue and gray." src={linkedin} /></a>
 
-                    <Link href="https://sarahpstudio.medium.com/" target="_blank" rel="noopener noreferrer"><img className="social-img medium align-bottom" src={medium} alt="A large letter M in blue and gray." /></Link>
+                    <a href="https://sarahpstudio.medium.com/" target="_blank" rel="noopener noreferrer"><img className="social-img medium align-bottom" src={medium} alt="A large letter M in blue and gray." /></a>
 
                   </div>
                   <div className="navigation">
                     <ul className="nav">
-                      <li className="nav-item"><Link to="https://sarahpstudio.s3.amazonaws.com/SarahPeonyPrasuhnResume-2.pdf" target="_blank" rel="noopener noreferrer">Resume</Link></li>
-                      <li className="nav-item"><Link to="https://github.com/sarahp" target="_blank" rel="noopener noreferrer">Github</Link></li>
+                      <li className="nav-item"><a href="https://sarahpstudio.s3.amazonaws.com/SarahPeonyPrasuhnResume-2.pdf" target="_blank" rel="noopener noreferrer">Resume PDF</a></li>
+
+                      <li className="nav-item"><Link to="/resume" target="_blank" rel="noopener noreferrer">Resume Text</Link></li>
+                      <li className="nav-item"><a to="https://github.com/sarahp" target="_blank" rel="noopener noreferrer">Github</a></li>
                       <li className="nav-item"><a href="mailto: me@sarahp.studio" target="_blank" rel="noopener noreferrer">Contact me</a></li>
-                      <li className="nav-item"><Link to="https://catfans.github.io/" target="_blank" rel="noopener noreferrer">#CatFanstheBand</Link></li>
+                      <li className="nav-item"><a to="https://catfans.github.io/" target="_blank" rel="noopener noreferrer">#CatFanstheBand</a></li>
 
                       {/*<li className="nav-item"><NavLink to="/portfolio">Portfolio</NavLink></li>*/}
                     </ul>
@@ -58,18 +60,18 @@ const IndexPage = () => {
                 built sites for major national brands. Created membership databases, basic retail sites, and beyond.
                 Recently I've been digging into React and using it with the MetAPI to explore alternate ways of viewing the collection online.</p>
               <h3>My favorite Programming Pieces.</h3>
-              <p><Link to="https://www.wscuc.org/" target="_blank" rel="noopener noreferrer">WASC</Link>. I completely redesigned and built the FrontEnd of this site. Using Bootstrap and Drupal 7. I am proud to say that it has stood the test of time.</p>
-              <Link to="https://www.wscuc.org/" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={wasc}></img></Link>
+              <p><a to="https://www.wscuc.org/" target="_blank" rel="noopener noreferrer">WASC</a>. I completely redesigned and built the FrontEnd of this site. Using Bootstrap and Drupal 7. I am proud to say that it has stood the test of time.</p>
+              <a to="https://www.wscuc.org/" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={wasc}></img></a>
 
 
-              <p><Link to="https://www.curf.upenn.edu/" target="_blank" rel="noopener noreferrer">Upenn Curf</Link>. For any site with a heavy CMS and many departments on going maintenance and front end fixes are a constant need. Working for <Link to="https://alphex.com/" target="_blank" rel="noopener noreferrer">Alephx</Link>, I helped with site fixes and front end development support. I love doing a blend of support and redesign work to keep my skills fresh.</p>
-              <Link to="https://www.curf.upenn.edu/" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={upenn}></img></Link>
+              <p><a to="https://www.curf.upenn.edu/" target="_blank" rel="noopener noreferrer">Upenn Curf</a>. For any site with a heavy CMS and many departments on going maintenance and front end fixes are a constant need. Working for <a to="https://alphex.com/" target="_blank" rel="noopener noreferrer">Alephx</a>, I helped with site fixes and front end development support. I love doing a blend of support and redesign work to keep my skills fresh.</p>
+              <a to="https://www.curf.upenn.edu/" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={upenn}></img></a>
 
-              <p><Link to="https://www.hunterboots.com/us/en_us/" target="_blank" rel="noopener noreferrer">Hunter Boots</Link>. In a white label collaboration. I managed the tech team, and built the front end. Like WASC this site has stood the test of time. And I'm proud that my work still lives selling my favorite boots.</p>
-              <Link to="https://www.hunterboots.com/us/en_us/" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={hunter}></img></Link>
+              <p><a to="https://www.hunterboots.com/us/en_us/" target="_blank" rel="noopener noreferrer">Hunter Boots</a>. In a white label collaboration. I managed the tech team, and built the front end. Like WASC this site has stood the test of time. And I'm proud that my work still lives selling my favorite boots.</p>
+              <a to="https://www.hunterboots.com/us/en_us/" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={hunter}></img></a>
 
-              <p><Link to="https://www.xepicream.com/" target="_blank" rel="noopener noreferrer">Xepi</Link>. Working with Isite, I built the front end of the Xepi site for launch. Making it accessible to the non-developer parts of the team for easy content updates.</p>
-              <Link to="https://www.xepicream.com/" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={xepi}></img></Link>
+              <p><a to="https://www.xepicream.com/" target="_blank" rel="noopener noreferrer">Xepi</a>. Working with Isite, I built the front end of the Xepi site for launch. Making it accessible to the non-developer parts of the team for easy content updates.</p>
+              <a to="https://www.xepicream.com/" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={xepi}></img></a>
 
 
 
@@ -84,17 +86,17 @@ const IndexPage = () => {
           <div className="navigation">
               <ul className="nav ">
                 <li className="nav-item"><a href="https://sarahpstudio.s3.amazonaws.com/SarahPeonyPrasuhnResume-2.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
-                <li className="nav-item"><Link href="https://github.com/sarahp" target="_blank" rel="noopener noreferrer">Github</Link></li>
+                <li className="nav-item"><a href="https://github.com/sarahp" target="_blank" rel="noopener noreferrer">Github</a></li>
                 <li className="nav-item"><a href="mailto: me@sarahp.studio" target="_blank" rel="noopener noreferrer">Contact me</a></li>
-                <li className="nav-item"><Link href="https://catfans.github.io/" target="_blank" rel="noopener noreferrer">#CatFanstheBand</Link></li>
+                <li className="nav-item"><a href="https://catfans.github.io/" target="_blank" rel="noopener noreferrer">#CatFanstheBand</a></li>
 
                 {/*<li className="nav-item"><NavLink to="/portfolio">Portfolio</NavLink></li>*/}
               </ul>
             </div>
             <div className="social-links">
-              <Link href="https://www.linkedin.com/in/sarahprasuhn/" target="_blank" rel="noopener noreferrer"><img className="social-img linked-in align-bottom" alt="A Linkedin logo in blue and gray." src={linkedin} /></Link>
-              <Link href="https://sarahpstudio.medium.com/" target="_blank" rel="noopener noreferrer"><img className="social-img medium align-bottom" src={medium} alt="A large letter M in blue and gray." /></Link>
-              {/*<Link href="https://www.youtube.com/channel/UCUFeOswQrFcLBjYrEc5w58Q" target="_blank" rel="noopener noreferrer"><img className="social-img youtube align-bottom" alt="A youtube logo in blue and gray." src={youtube} /></Link>*/}
+              <a href="https://www.linkedin.com/in/sarahprasuhn/" target="_blank" rel="noopener noreferrer"><img className="social-img linked-in align-bottom" alt="A Linkedin logo in blue and gray." src={linkedin} /></a>
+              <a href="https://sarahpstudio.medium.com/" target="_blank" rel="noopener noreferrer"><img className="social-img medium align-bottom" src={medium} alt="A large letter M in blue and gray." /></a>
+              {/*<a href="https://www.youtube.com/channel/UCUFeOswQrFcLBjYrEc5w58Q" target="_blank" rel="noopener noreferrer"><img className="social-img youtube align-bottom" alt="A youtube logo in blue and gray." src={youtube} /></a>*/}
             </div>
           </div>
           <div className="copyright">©SarahP.Studio(Sarah Prasuhn)</div>
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,110 +1,94 @@
-import * from 'react';
+import React from "react"
 
-<div clasName="resume-text">
-  SARAH PEONY (PRASUHN)
+const Resume = () => {
+  return (
 
-  me@sarahp.studio
-  (267) 225-4629
-  SarahP.studio (portfolio)
-  Philadelphia/NYC
+    <div className="resume-text">
 
-  TECH STACK
-  ======
-  - ES6
-  - React
-  - Sass
-  - HTML/CSS
-  - Node.JS
-  - Javascript
-  - Rails
-  - Middleman
-  - Various CMS'
-  - MySql
-  - Ruby
+      <p>SARAH PEONY (PRASUHN)</p>
 
+      <p>me@sarahp.studio (267) 225-4629 SarahP.studio (portfolio) Philadelphia/NYC</p>
+      <br></br>
 
+      <p>TECH STACK </p>
+      <p>======</p>
+      <p> - ES6 - React - Sass - HTML/CSS - Node.JS - Javascript - Rails - Middleman - Various CMS' - MySql - Ruby</p>
+      <br></br>
 
-  Strengths
-  ======
-  11yrs of Development Experience
-  I started coding way back in 2007, and have gone through a myriad of tools and techniques since. This has given me a good intuition on what is worth pursuing, and where there might be pitfalls.
+      <p>Strengths </p>
+      <p>======</p>
+      <p> 11yrs of Development Experience I started coding way back in 2007, and have gone through a myriad of tools and
+        techniques since. This has given me a good intuition on what is worth pursuing, and where there might be
+        pitfalls.</p>
 
-  Development is a team effort
-  Knowing where to bring in my skills on a project, and how best to support people in our shared goals.
+      <p>Development is a team effort Knowing where to bring in my skills on a project, and how best to support people
+        in our shared goals.</p>
 
-  Presentations & Public speaking are second nature
-  Being a Communications major has its perks. I'm comfortable presenting high concepts in a friendly way, to any size crowd.
+      <p>Presentations &amp; Public speaking are second nature Being a Communications major has its perks. I'm
+        comfortable presenting high concepts in a friendly way, to any size crowd.</p>
+      <br></br>
+      <p> Experience</p>
+      <p> ======</p>
+      <p> Front End Programmer Shomeya/SarahP.Studio 2008 - Ongoing Portland, Oregon/Philadelphia Boutique web
+        development agency I ran from the ground up. I was the Lead Front End Programmer the entire time. - Transitioned
+        from to React as my main programming tool. - Juggled clients, devs, and the Open Source community's needs, while
+        still writing my own quality code. - Kept a small team of developers working in sync to build enterprise level
+        sites, such as Hunter Boots US. - Focused on logical interfaces and sustainable code. - Trained and educated
+        other developers on how to create code that lasts. - My &quot;The Client Calls at Midnight&quot; presentation
+        still gets mentions years later - Wrote two visual guides on how to adapt Bootstrap into Drupal themes.</p>
+      <br></br>
 
+      <p> INTERESTS &amp; CREATIVE WORK</p>
+      <p> ======</p>
+      <p> Acid Love Music Video Visualist/Video producer Sarah P &amp; Music @pamsaur (twitter) 2018 - Ongoing
+        https://catfans.github.io Livecoding is using code to produce a live art experience. CatFans is a chill band
+        that performs for everything from Tech Conferences to Synth Experiences. It's such an amazing experience to code
+        for all to see, and make art in the same moment.</p>
+      <br></br>
 
-  Experience
-  ======
-  Front End Programmer
-  Shomeya/SarahP.Studio
-  2008 - Ongoing
-  Portland, Oregon/Philadelphia
-  Boutique web development agency I ran from the ground up. I was the Lead Front End Programmer the entire time.
-  - Transitioned from to React as my main programming tool.
-  - Juggled clients, devs, and the Open Source community's needs, while still writing my own quality code.
-  - Kept a small team of developers working in sync to build enterprise level sites, such as Hunter Boots US.
-  - Focused on logical interfaces and sustainable code.
-  - Trained and educated other developers on how to create code that lasts.
-  - My "The Client Calls at Midnight" presentation still gets mentions years later
-  - Wrote two visual guides on how to adapt Bootstrap into Drupal themes.
+      <p> LEARN MORE </p>
+      <p> ======</p>
+      <p> Github https://github.com/sarahp</p>
 
+      <p>LinkedIN sarahprasuhn</p>
 
-  INTERESTS & CREATIVE WORK
-  ======
-  Acid Love Music Video
-  Visualist/Video producer Sarah P & Music @pamsaur (twitter)
-  2018 - Ongoing
-  https://catfans.github.io
-  Livecoding is using code to produce a live art experience. CatFans is a chill band that performs for everything from Tech Conferences to Synth Experiences. It's such an amazing experience to code for all to see, and make art in the same moment.
+      <p>Medium @sarahpstudio</p>
 
+      <p>Twitter @sarahpstudio</p>
+      <br></br>
 
-  LEARN MORE
-  ======
-  Github
-  https://github.com/sarahp
+      <p> CODING EXAMPLES</p>
+      <p> ======</p>
+      <p> Front End Development favorites UPENN (2017) &mdash; Through Alphex (Duran Goodyear) Support Frontend
+        Developer https://www.curf.upenn.edu/</p>
 
-  LinkedIN
-  sarahprasuhn
+      <p>WASC Completed the redesign and the recode. https://www.wscuc.org</p>
 
-  Medium
-  @sarahpstudio
+      <p>Hunter Boots Developed the front end of their online catalog debut. https://www.hunterboots.com/us/en_us/</p>
 
-  Twitter
-  @sarahpstudio
+      <p>Under construction Synesthesia I&rsquo;m also working on using theMetAPI to recreate the experiences of
+        synesthesia that I have when I view works at theMet in NYC.</p>
+      <br></br>
 
+      <p> VOLUNTEER </p>
+      <p>======</p>
+      <p> Open Source Contributor Drupal.org 2008 - 2016 As part of Drupal's growth from a small community to several
+        thousand. Community challenges always accompany growth, and I helped develop strategies to encourage mentorship
+        and inclusivity. I also contributed to Drupal.org's frontend code base.</p>
+      <br></br>
 
-  CODING EXAMPLES
-  ======
-  Front End Development favorites
-  UPENN (2017) — Through Alphex (Duran Goodyear) Support Frontend Developer
-  https://www.curf.upenn.edu/
+      <p> Education </p>
+      <p> ======</p>
+      <p>B.A. in Communications and Theater/Film Indiana Wesleyan University 08/2000 - 12/2003</p>
 
-  WASC
-  Completed the redesign and the recode. https://www.wscuc.org
+      <p></p>
 
-  Hunter Boots
-  Developed the front end of their online catalog debut. https://www.hunterboots.com/us/en_us/
+      <li className="nav-item"><a href="https://sarahpstudio.s3.amazonaws.com/SarahPeonyPrasuhnResume-2.pdf"
+                                  target="_blank" rel="noopener noreferrer">Resume PDF Version</a></li>
 
-  Under construction Synesthesia
-  I’m also working on using theMetAPI to recreate the experiences of synesthesia that I have when I view works at theMet in NYC.
+    </div>
 
+  )
+}
 
-  VOLUNTEER
-  ======
-  Open Source Contributor
-  Drupal.org
-  2008 - 2016
-  As part of Drupal's growth from a small community to several thousand. Community challenges always accompany growth, and I helped develop strategies to encourage mentorship and inclusivity. I also contributed to Drupal.org's frontend code base.
-
-
-  Education
-  ======
-  B.A. in Communications and Theater/Film
-  Indiana Wesleyan University
-  08/2000 - 12/2003
-
-
-</div>
+export default Resume
