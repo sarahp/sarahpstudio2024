13c4e55bfd4dab810d784650898c3a51be4768ac
sarahp <me@sarahp.studio>
2021-02-15 20:56:05 -0500

    Including Marni's updates and adding in media queries.

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,23 +25,22 @@ const IndexPage = () => {
                 </div>
               <div className="header-inner">
                   <div className="social-links">
-                    <Link href="https://sarahpstudio.medium.com/" target="_blank" rel="noopener noreferrer"><img className="social-img medium align-bottom" src={medium} alt="A large letter M in blue and gray." /></Link>
                     <Link href="https://www.linkedin.com/in/sarahprasuhn/" target="_blank" rel="noopener noreferrer"><img className="social-img linked-in align-bottom" alt="A Linkedin logo in blue and gray." src={linkedin} /></Link>
-                    <Link href="https://www.youtube.com/channel/UCUFeOswQrFcLBjYrEc5w58Q" target="_blank" rel="noopener noreferrer"><img className="social-img youtube align-bottom" alt="A youtube logo in blue and gray." src={youtube} /></Link>
+
+                    <Link href="https://sarahpstudio.medium.com/" target="_blank" rel="noopener noreferrer"><img className="social-img medium align-bottom" src={medium} alt="A large letter M in blue and gray." /></Link>
+
                   </div>
                   <div className="navigation">
                     <ul className="nav">
-                      <li className="nav-item"><Link to="https://www.dropbox.com/s/lxtb4b2m7pbqqzw/SarahPeonyPrasuhnResume-2.pdf?dl=0.pdf?dl=0">Resume</Link></li>
-                      <li className="nav-item"><a href="mailto: me@sarahp.studio">Contact me</a></li>
-                      <li className="nav-item"><Link to="https://catfans.github.io/">#CatFanstheBand</Link></li>
+                      <li className="nav-item"><Link to="https://sarahpstudio.s3.amazonaws.com/SarahPeonyPrasuhnResume-2.pdf" target="_blank" rel="noopener noreferrer">Resume</Link></li>
+                      <li className="nav-item"><Link to="https://github.com/sarahp" target="_blank" rel="noopener noreferrer">Github</Link></li>
+                      <li className="nav-item"><a href="mailto: me@sarahp.studio" target="_blank" rel="noopener noreferrer">Contact me</a></li>
+                      <li className="nav-item"><Link to="https://catfans.github.io/" target="_blank" rel="noopener noreferrer">#CatFanstheBand</Link></li>
 
                       {/*<li className="nav-item"><NavLink to="/portfolio">Portfolio</NavLink></li>*/}
                     </ul>
                   </div>
               </div>
-                  <div className="tagline">
-                    <h3>For over a decade, I've been living my best life programming applications and building stories.</h3>
-                  </div>
           </header>
 
           <section className="content display-project">
@@ -49,6 +48,9 @@ const IndexPage = () => {
             </div>
 
             <div className="frontpage-portfolio">
+              <div className="tagline">
+                <h3>For over a decade, I've been living my best life programming applications and building stories.</h3>
+              </div>
 
               <div className="fe-header"> <h2>Front End Programming</h2></div>
 
@@ -81,17 +83,18 @@ const IndexPage = () => {
           <div className="footer-navigation">
           <div className="navigation">
               <ul className="nav ">
-                <li className="nav-item"><Link to="https://www.dropbox.com/s/lxtb4b2m7pbqqzw/SarahPeonyPrasuhnResume-2.pdf?dl=0.pdf?dl=0">Resume</Link></li>
-                <li className="nav-item"><a href="mailto: me@sarahp.studio">Contact me</a></li>
-                <li className="nav-item"><Link to="https://catfans.github.io/">#CatFanstheBand</Link></li>
+                <li className="nav-item"><a href="https://sarahpstudio.s3.amazonaws.com/SarahPeonyPrasuhnResume-2.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
+                <li className="nav-item"><Link href="https://github.com/sarahp" target="_blank" rel="noopener noreferrer">Github</Link></li>
+                <li className="nav-item"><a href="mailto: me@sarahp.studio" target="_blank" rel="noopener noreferrer">Contact me</a></li>
+                <li className="nav-item"><Link href="https://catfans.github.io/" target="_blank" rel="noopener noreferrer">#CatFanstheBand</Link></li>
 
                 {/*<li className="nav-item"><NavLink to="/portfolio">Portfolio</NavLink></li>*/}
               </ul>
             </div>
             <div className="social-links">
-              <Link href="https://sarahpstudio.medium.com/" target="_blank" rel="noopener noreferrer"><img className="social-img medium align-bottom" src={medium} alt="A large letter M in blue and gray." /></Link>
               <Link href="https://www.linkedin.com/in/sarahprasuhn/" target="_blank" rel="noopener noreferrer"><img className="social-img linked-in align-bottom" alt="A Linkedin logo in blue and gray." src={linkedin} /></Link>
-              <Link href="https://www.youtube.com/channel/UCUFeOswQrFcLBjYrEc5w58Q" target="_blank" rel="noopener noreferrer"><img className="social-img youtube align-bottom" alt="A youtube logo in blue and gray." src={youtube} /></Link>
+              <Link href="https://sarahpstudio.medium.com/" target="_blank" rel="noopener noreferrer"><img className="social-img medium align-bottom" src={medium} alt="A large letter M in blue and gray." /></Link>
+              {/*<Link href="https://www.youtube.com/channel/UCUFeOswQrFcLBjYrEc5w58Q" target="_blank" rel="noopener noreferrer"><img className="social-img youtube align-bottom" alt="A youtube logo in blue and gray." src={youtube} /></Link>*/}
             </div>
           </div>
           <div className="copyright">©SarahP.Studio(Sarah Prasuhn)</div>
---
+++ src/pages/resume.js
@@ -0,0 +1,110 @@
+import * from 'react';
+
+<div clasName="resume-text">
+  SARAH PEONY (PRASUHN)
+
+  me@sarahp.studio
+  (267) 225-4629
+  SarahP.studio (portfolio)
+  Philadelphia/NYC
+
+  TECH STACK
+  ======
+  - ES6
+  - React
+  - Sass
+  - HTML/CSS
+  - Node.JS
+  - Javascript
+  - Rails
+  - Middleman
+  - Various CMS'
+  - MySql
+  - Ruby
+
+
+
+  Strengths
+  ======
+  11yrs of Development Experience
+  I started coding way back in 2007, and have gone through a myriad of tools and techniques since. This has given me a good intuition on what is worth pursuing, and where there might be pitfalls.
+
+  Development is a team effort
+  Knowing where to bring in my skills on a project, and how best to support people in our shared goals.
+
+  Presentations & Public speaking are second nature
+  Being a Communications major has its perks. I'm comfortable presenting high concepts in a friendly way, to any size crowd.
+
+
+  Experience
+  ======
+  Front End Programmer
+  Shomeya/SarahP.Studio
+  2008 - Ongoing
+  Portland, Oregon/Philadelphia
+  Boutique web development agency I ran from the ground up. I was the Lead Front End Programmer the entire time.
+  - Transitioned from to React as my main programming tool.
+  - Juggled clients, devs, and the Open Source community's needs, while still writing my own quality code.
+  - Kept a small team of developers working in sync to build enterprise level sites, such as Hunter Boots US.
+  - Focused on logical interfaces and sustainable code.
+  - Trained and educated other developers on how to create code that lasts.
+  - My "The Client Calls at Midnight" presentation still gets mentions years later
+  - Wrote two visual guides on how to adapt Bootstrap into Drupal themes.
+
+
+  INTERESTS & CREATIVE WORK
+  ======
+  Acid Love Music Video
+  Visualist/Video producer Sarah P & Music @pamsaur (twitter)
+  2018 - Ongoing
+  https://catfans.github.io
+  Livecoding is using code to produce a live art experience. CatFans is a chill band that performs for everything from Tech Conferences to Synth Experiences. It's such an amazing experience to code for all to see, and make art in the same moment.
+
+
+  LEARN MORE
+  ======
+  Github
+  https://github.com/sarahp
+
+  LinkedIN
+  sarahprasuhn
+
+  Medium
+  @sarahpstudio
+
+  Twitter
+  @sarahpstudio
+
+
+  CODING EXAMPLES
+  ======
+  Front End Development favorites
+  UPENN (2017) — Through Alphex (Duran Goodyear) Support Frontend Developer
+  https://www.curf.upenn.edu/
+
+  WASC
+  Completed the redesign and the recode. https://www.wscuc.org
+
+  Hunter Boots
+  Developed the front end of their online catalog debut. https://www.hunterboots.com/us/en_us/
+
+  Under construction Synesthesia
+  I’m also working on using theMetAPI to recreate the experiences of synesthesia that I have when I view works at theMet in NYC.
+
+
+  VOLUNTEER
+  ======
+  Open Source Contributor
+  Drupal.org
+  2008 - 2016
+  As part of Drupal's growth from a small community to several thousand. Community challenges always accompany growth, and I helped develop strategies to encourage mentorship and inclusivity. I also contributed to Drupal.org's frontend code base.
+
+
+  Education
+  ======
+  B.A. in Communications and Theater/Film
+  Indiana Wesleyan University
+  08/2000 - 12/2003
+
+
+</div>
