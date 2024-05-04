e35291ee4252c94c5ddf97ffa89e2c803b40d45a
Sarah P <lovesarahinc@gmail.com>
2024-05-04 02:17:42 -0400

    switching to nextJS

---
+++ .cache/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/sarahpeony/git/sarahpstudio2021/.cache/dev-404-page.js"))
+}
+
