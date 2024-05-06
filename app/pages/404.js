41366c2e7183636a24e6ce3464a2745f31d7a3d4
Sarah P <lovesarahinc@gmail.com>
2024-05-06 13:49:30 -0400

    fix(spstudio): small change to trigger netlify build

--- app/pages/404.js
+++ app/pages/404.js
@@ -28,7 +28,7 @@ const codeStyles = {
 const NotFoundPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Not found</title>
+      <title>Page Not found</title>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
         Sorry{" "}
