c0ad0303f1ee23480da52f0644f21237ac79af45
Sarah P <lovesarahinc@gmail.com>
2024-05-06 17:17:10 -0400

    feat(spstudio): working on the app router pages

---
+++ app/resume/resume.js
@@ -0,0 +1,137 @@
+import React from "react";
+import "../styles/styles.scss";
+
+const Resume = () => {
+  return (
+    <div>
+      <h1>SARAH PEONY (PRASUHN)</h1>
+      <p>lovesarahinc@gmail.com</p>
+      <p>503-539-7107 [prefer text]</p>
+      <p><a href="https://SarahP.studio">SarahP.studio</a> (portfolio)</p>
+      <p>Philadelphia/NYC</p>
+
+      <h2>WHY I LOVE WHAT I DO</h2>
+      <p>
+        With a unique background that combines creative storytelling and technical expertise, I bring a fresh perspective to the field of frontend development. Math becoming art and the human experience, is why I got into Web Development in the first place. Hailing from the world of post-production editing, I have honed a meticulous eye for detail and a knack for conveying intricate narratives through engaging visuals. This foundation in visual storytelling translates seamlessly into crafting captivating user experiences that seamlessly cross the gap between complex mathematical algorithms and intuitive interfaces. Beyond my primary role, I am deeply invested in a personal side project that integrates OpenAI with NextJS to create a chore app tailored to meet the unique needs of neurodiverse individuals. This endeavor underscores my commitment to staying at the forefront of technological advancements and my passion for exploring the limitless possibilities of AI in enhancing user experiences. I&aposm a passionate, dedicated, innovative game-changer that would love to join Gurobi as it uses Math to make the world a better place.
+      </p>
+
+      <h2>SKILLS</h2>
+      <h3>Coding</h3>
+      <ul>
+        <li>Lit</li>
+        <li>NextJS</li>
+        <li>Typescript</li>
+        <li>Storybook</li>
+        <li>APIs</li>
+        <li>ReactJS</li>
+        <li>CMS</li>
+        <li>Javascript — E6</li>
+        <li>Software Development</li>
+        <li>HTML/CSS</li>
+        <li>Jenkins</li>
+        <li>Webpack</li>
+        <li>Cloud Services</li>
+        <li>SASS</li>
+        <li>Design Patterns</li>
+        <li>Docker</li>
+        <li>Flux</li>
+        <li>Restful APIs</li>
+        <li>npm</li>
+        <li>React Hooks</li>
+        <li>Unit Testing</li>
+      </ul>
+
+      <h3>Process & Non-coding</h3>
+      <ul>
+        <li>Agile methodologies</li>
+        <li>Project Management</li>
+        <li>Quality Assurance</li>
+        <li>Jira</li>
+        <li>Git</li>
+        <li>Google Drive</li>
+        <li>Confluence</li>
+        <li>UX Design</li>
+        <li>Web-design</li>
+        <li>Product Roadmap</li>
+        <li>Business Strategy</li>
+        <li>AI Prompt Engineering</li>
+        <li>Debugging</li>
+      </ul>
+
+      <h2>Experience</h2>
+      <h3>Software Developer</h3>
+      <p>Phase2</p>
+      <p>2021 - 2024</p>
+      <p>Remote (Philadelphia)</p>
+      <p>Enterprise level agency</p>
+      <ul>
+        <li>Created over 100 customized components written in Typescript, ES6, MDX</li>
+        <li>Trained teammates on 4 different projects on best practices for Outline/Web-component development</li>
+        <li>Presented at client meetings their customized Outline/Web-Component design system implementation</li>
+        <li>Worked closely with backend developers to integrate Web-Components smoothly into consumers, Drupal, React etc</li>
+        <li>Integrated JS Libraries into Lit Web Components written in TS</li>
+        <li>Presented on Coding as Art at the annual company meeting/Conference</li>
+      </ul>
+
+      <h3>Front End Programmer</h3>
+      <p>Shomeya/SarahP.Studio</p>
+      <p>2008 - 2021</p>
+      <p>Portland, Oregon/Philadelphia</p>
+      <p>Boutique web development agency I ran from the ground up. I was the Lead Front End Programmer & Principal for the duration.</p>
+      <ul>
+        <li>Launched a cutting edge video platform for a BTS look at Kanye West&aposs team</li>
+        <li>Used Shopify to help my clients sell their personal brands</li>
+        <li>Wrote books and trained clients on using Documentary style social media to grow their businesses, which worked well for my clients</li>
+        <li>Kept a small team of developers working in sync to build enterprise level sites, such as Hunter Boots US</li>
+        <li>My &quotThe Client Calls at Midnight&quot presentation still gets mentions years later</li>
+        <li>Wrote two visual guides on how to adapt Bootstrap into Drupal themes</li>
+      </ul>
+
+      <h3>Lead Assistant Editor Bridezillas</h3>
+      <p>September Films</p>
+      <p>2004undefined</p>
+      <p>Los Angeles</p>
+      <p>Production Studio</p>
+      <ul>
+        <li>Reviewed and input over 300hrs of footage for the Editors</li>
+        <li>Created 10 Rough cuts of the tapes, so the editors could get to work faster</li>
+        <li>Took notes of important moments for producers, to get the best cuts</li>
+        <li>Established strong communication with the Editors even though they worked opposite hours</li>
+        <li>Kept my manager up to date in spite of opposite schedules</li>
+      </ul>
+
+      <h2>INTERESTS & CREATIVE WORK</h2>
+      <h3>Acid Love Music Video</h3>
+      <p>Visualist/Video producer Sarah P & Music @pamsaur (twitter)</p>
+      <p>2018 - Ongoing</p>
+      <p><a href="https://catfans.github.io">https://catfans.github.io</a></p>
+      <p>Live coding is using code to produce a live art experience. CatFans is a chill band that performs for everything from Tech Conferences to Synth Experiences. It&aposs such an amazing experience to code for all to see, and make art in the same moment.</p>
+
+      <h2>LEARN MORE</h2>
+      <p>GitHub: <a href="https://github.com/sarahp">https://github.com/sarahp</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/sarahpeony">sarahpeony</a></p>
+      <p>Medium: <a href="https://medium.com/sarahp-studio">https://medium.com/sarahp-studio</a></p>
+      <p>Twitter: <a href="https://twitter.com/sarahpstudio">@sarahpstudio</a></p>
+
+      <h2>Strengths</h2>
+      <ul>
+        <li>Development is a team effort - Knowing where to bring in my skills on a project, and how best to support people in our shared goals.</li>
+        <li>10+yrs of Development Experience - Skilled professional with 10+ years&apos experience, adept at driving innovation and delivering exceptional results through continuous growth.</li>
+        <li>Engagement is my favorite - Being a Communications major has its perks. I&aposm comfortable presenting and working with product and user needs in mind.</li>
+      </ul>
+
+      <h2>VOLUNTEER</h2>
+      <p>Open Source Contributor</p>
+      <p>Drupal.org</p>
+      <p>2008 - 2016</p>
+      <p>Part of Drupal&aposs growth from a small community to several thousand. Community challenges always accompany growth, and I helped develop strategies to encourage mentorship and inclusivity. I also contributed to Drupal.org&aposs frontend code base.</p>
+
+      <h2>Education</h2>
+      <p>B.A. in Communications and Theater/Film</p>
+      <p>Indiana Wesleyan University</p>
+      <p>08/2000 - 12/2003</p>
+    </div>
+  );
+};
+
+export default Resume;
